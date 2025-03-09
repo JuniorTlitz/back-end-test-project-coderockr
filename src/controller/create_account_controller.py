9a4b983dc283c805070f19b3b57ae4ee2d933c16
Tarcizio Santos <tarciziosantos.dev@gmail.com>
2025-03-09 15:48:21 -0300

    feat: enhance CreateAccountController to support asynchronous email validation and account creation

--- src/controller/create_account_controller.py
+++ src/controller/create_account_controller.py
@@ -1,19 +1,57 @@
 from abc import ABC, abstractmethod
 from http import HTTPStatus
-from typing import Any
+from pydantic import BaseModel
 
+from src.models.authentication_model import AuthenticationModel
+from src.models.create_account_db_model import CreateAccountDbModel
+from src.usecases.authentication import Authentication
+from src.usecases.create_account_db import CreateAccountDb
+from src.usecases.email_already_exists import EmailAlreadyExists
 
-class EmailAlreadyExists(ABC):
-    @abstractmethod
-    def validate_email(self, email: str) -> bool:
-        pass
 
 class CreateAccountController:
-    def __init__(self, email_already_exists: EmailAlreadyExists):
+
+    def __init__(
+        self,
+        email_already_exists: EmailAlreadyExists,
+        create_account_db: CreateAccountDb,
+        authentication: Authentication,
+    ):
         self._email_already_exists = email_already_exists
+        self._create_account_db = create_account_db
+        self._authentication = authentication
 
     async def handle(self, request):
-        is_valid = self._email_already_exists.validate_email(request['email'])
+        email_exists = await self._email_already_exists.validate_email(request["email"])
 
-        if is_valid:
-            return {'status_code': HTTPStatus.FORBIDDEN, 'message': 'Account already exists'}
+        if email_exists:
+            return {
+                "status_code": HTTPStatus.FORBIDDEN,
+                "message": "Account already exists",
+            }
+
+        account = await self._create_account_db.create_account(
+            CreateAccountDbModel(
+                name=request["name"],
+                email=request["email"],
+                password=request["password"],
+            )
+        )
+
+        if not account:
+            return {
+                "status_code": HTTPStatus.INTERNAL_SERVER_ERROR,
+                "message": "Internal server error",
+            }
+
+        auth = await self._authentication.auth(
+            AuthenticationModel(
+                email=request["email"],
+                password=request["password"],
+            )
+        )
+
+        return {
+            "status_code": HTTPStatus.CREATED,
+            "body": auth,
+        }

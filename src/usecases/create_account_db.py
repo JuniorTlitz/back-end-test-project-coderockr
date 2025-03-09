593b4c581b0a39f5d2f770116476de5bfba5d061
Tarcizio Santos <tarciziosantos.dev@gmail.com>
2025-03-09 15:47:32 -0300

    refactor: change create_account method to be asynchronous in CreateAccountDb

--- src/usecases/create_account_db.py
+++ src/usecases/create_account_db.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Awaitable
 
 from src.models.create_account_db_model import CreateAccountDbModel
 
 
 class CreateAccountDb(ABC):
     @abstractmethod
-    def create_account(self, account: CreateAccountDbModel):
+    async def create_account(self, account: CreateAccountDbModel) -> Awaitable[bool]:
         pass
